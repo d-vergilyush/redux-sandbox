967c2efa157ee31b533fa0a72c55b48f46ebedf4
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-13 00:51:29 +0300

    Refactoring `index.js`

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
 import * as actions from './actions';
 
+import Counter from './counter';
+
 const store = createStore(reducer, 0);
 const { dispatch } = store;
 
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-document
-  .querySelector('.inc')
-  .addEventListener('click', () => inc());
-document
-  .querySelector('.dec')
-  .addEventListener('click', () => dec());
-document
-  .querySelector('.rnd')
-  .addEventListener('click', () => {
-    const payload = Math.floor(Math.random()*10);
-    rnd(payload);
-  });
+const { inc, dec, rnd } = 
+  bindActionCreators(actions, dispatch);
   
 const update = () => {
-  document
-    .querySelector('.counter')
-    .innerHTML = store.getState()
-}
+  ReactDOM.render(
+    <Counter 
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+      }} />, document.getElementById('root')
+  );
+};
+
+update();
 store.subscribe(() => update());
-
-
-
