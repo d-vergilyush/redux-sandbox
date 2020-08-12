efeaa234d7301eee5a17cabded6f409d97324cbe
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-12 18:09:12 +0300

    Сhange basic structure

--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
 import { createStore } from 'redux';
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-
-    case 'DEC':
-      return state - 1;
-
-    case 'RND':
-      return state + action.payload;
-  
-    default:
-      return state;
-  }
-}
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer, 0);
-
-const inc = () => ({ type: 'INC' });
-
-const dec = () => ({ type: 'DEC' });
-
-const rnd = (payload) => ({ type: 'RND', payload });
+const { dispatch } = store;
 
 document
   .querySelector('.inc')
   .addEventListener('click', () => {
-    store.dispatch(inc());
+    dispatch(inc());
   });
 document
   .querySelector('.dec')
   .addEventListener('click', () => {
-    store.dispatch(dec());
+    dispatch(dec());
   });
 document
   .querySelector('.rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random()*10)
-    store.dispatch(rnd(payload));
+    dispatch(rnd(payload));
   });
   
 const update = () => {
