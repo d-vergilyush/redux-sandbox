d1c4afd4fb65a94fc566fb863c6820e0737f4164
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-15 03:05:50 +0300

    Refactoring `Counter`. Use `connect`

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actionCreators';
 
 const Counter = ({ counter, inc, dec, rnd }) => {
   return (
@@ -23,4 +26,14 @@ const Counter = ({ counter, inc, dec, rnd }) => {
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
